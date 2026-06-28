ffb08a593d06327a1f392cea13b809e958a6b856
YuKiMK <proguraming1023@gmail.com>
2026-06-28 19:12:18 +0900

    Delete 科学情報 directory

--- 科学情報/run_morning.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python update_news.py --edition morning >> update_log.txt 2>&1
--- 科学情報/run_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo.
-echo  Cosmos News サーバーを起動します...
-echo  IPアドレスは起動後の画面に表示されます。
-echo  このウィンドウは開いたままにしてください。
-echo.
-python server.py
-pause
